597e37d533ffda69fea8a698d89b5ae665e1b811
Ashley Ricketts <aricketts12@gmail.com>
2019-02-18 11:21:20 -0600

    created crystalNumber function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,32 @@
+
+var randomNum;
+
+
+
+// Creates random number for player to add to
 function randomNumber(min, max){
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
 
 }
 
-console.log(randomNumber(19, 120));
+// Writes randomNumber to document 
+randomNum = document.getElementById("randomNum");
+randomNum.innerHTML = "Add up to " + randomNumber(19, 120) + " to win!";
+
+
+
+function crystalNumber(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+console.log(crystalNumber(1, 12));
+
+
 
 
 
