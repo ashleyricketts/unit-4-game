d67c9923ec93d806c089cd691681360dfd0c817f
Ashley Ricketts <aricketts12@gmail.com>
2019-02-20 18:54:36 -0600

    javascript edit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -66,10 +66,6 @@ $(document).ready(function() {
             }
             
     });
-
-    //game runs according to instructions but it is very hard to win, 
-    //I am working on how to assign crystals the random numbers under certain conditions so that it is possible to win every time...
-    
     
 
 
